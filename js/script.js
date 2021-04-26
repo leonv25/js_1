41f15ae9728bf7740ef3b4a453bc3ccaa8ad9483
LeonV <leonvv25@gmail.com>
2021-04-27 00:04:17 +0300

    commit_27.04.2021 00:04

--- js/script.js
+++ js/script.js
@@ -20,9 +20,23 @@ for (let i = 0; i < 2; i++) {
         && a != '' && b != '' && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
-        } else {
+    } else {
+
+        for (let i = 0; i > -2; i++) {
+            let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	            b = prompt("Во сколько обойдется?", '');
+            if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) === 'string' && (typeof(b)) != null 
+                && a != '' && b != '' && a.length < 50) {
+                    break;
+                    console.log("done");
+                    appData.expenses[a] = b;
+            }
+                
 
         }
+        
+        
+    }
     
 };
 
