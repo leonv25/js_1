81639986290587e548455efc0f5b0e1dc6961506
LeonV <leonvv25@gmail.com>
2021-04-28 22:20:09 +0300

    commit_28.04.2021 22:20

---
+++ js_test.js
@@ -0,0 +1,41 @@
+// let num = 20;
+
+// function showFirstMessege(text) {
+//         console.log(num);
+// }
+// showFirstMessege("Hello World");
+// console.log(num);
+// console.log(calc(3,4));
+// console.log(calc(8,4));
+
+// function calc(a,b) {
+//     return (a+b);
+// }
+
+// console.log(calc(3,4));
+// console.log(calc(8,4));
+
+// function retVar() {
+//     let num = 50;
+//     return num;
+// }
+
+// console.log(calc(3,4));
+// console.log(calc(8,4));
+
+// let calc = function (a,b) {
+//     return (a+b);
+// }
+
+// let str = "test";
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str.length);
+
+// let twelve = "12.2";
+// console.log(Math.round(twelve));
+
+let twelve = "12.2px";
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
