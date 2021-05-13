60393818c1762721d41141d87750942370c2d4e5
LeonV <leonvv25@gmail.com>
2021-05-13 16:52:24 +0300

    commit_2_15_notcompl

--- js_test.js
+++ js_test.js
@@ -18,3 +18,15 @@ for (let key in options) {
     console.log('Свойтво ' + key + ' имеет значение ' + options[key]);
 }
 console.log(Object.keys(options).length);
+
+let arr = ["first", 2, 3, "four", 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+arr.forEach(function(item, i, mass){
+    console.log(i + ': ' + item + " (масив: " + mass + ')');
+});
+
+console.log(arr);
