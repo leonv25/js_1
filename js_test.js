bd59576cbd3dd952f79b1726b50a1ca9efe4f9ff
LeonV <leonvv25@gmail.com>
2021-05-20 16:53:32 +0300

    commit_2_23__14:59_notcompl

---
+++ js_test.js
@@ -0,0 +1,27 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart');
+
+box.style.backgroundColor = 'blue';
+btn[1].style.borderRadius = '100%';
+
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+// heart.forEach(function(item, i, hearts){
+//     item.style.backgroundColor = 'blue';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+console.log(div);
