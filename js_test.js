00b81da290b7d0de3490acc968e2b091ed0c8533
LeonV <leonvv25@gmail.com>
2021-05-21 15:09:16 +0300

    commit_2_23_compl

--- js_test.js
+++ js_test.js
@@ -2,7 +2,9 @@ let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart');
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper')
+    ;
 
 box.style.backgroundColor = 'blue';
 btn[1].style.borderRadius = '100%';
@@ -23,5 +25,16 @@ let div = document.createElement('div'),
     text = document.createTextNode('Тут был я');
 
 div.classList.add('black');
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+
+// div.innerHTML = '<h1>Hello World</h1>';
+div.textContent = 'Hello World';
+
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(btn[1], circle[1]);
 
 console.log(div);
