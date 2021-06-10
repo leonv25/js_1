b8461505015ff935dad124251ca7d7ab1bbc4f6b
LeonV <leonvv25@gmail.com>
2021-06-10 20:44:51 +0300

    commit_10.06.2021

--- js_test.js
+++ js_test.js
@@ -1,37 +1,6 @@
-let btn = document.querySelectorAll('button'),
-    wrap = document.querySelector('.wrapper'),
-    link = document.querySelector('a');
-// btn[0].onclick = function() {
-//     alert('Вы нажали первую кнопку!');
-// };
-// btn[0].addEventListener('click', function(event) {
-//     let target = event.target;
-//     target.style.display = 'none';
-//     console.log('Произошло событие' + event.type + ' на элементе ' + event.target);
-// });
-// wrap.addEventListener('click', function(event) {
-//     console.log('Произошло событие' + event.type + ' на элементе ' + event.target);
-// });
-
-link.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('Произошло событие' + event.type + ' на элементе ' + event.target);
-});
-btn.forEach(function(item) {
-    item.addEventListener('mouseleave', function(event) {
-        console.log('Вышли');
-    });
-});
-// btn[0].addEventListener('click', function() {
-//     alert('Вы опять нажали первую кнопку!');
-// });
-
-// btn[0].addEventListener('mouseenter', function() {
-//     alert('Вы навели на первую кнопку');
-// });
+// touchstart
+// touchmove
+// touchend
 
 
-
-
-
-console.log(link);
+window.addEventListener('DOMContentLoaded')
